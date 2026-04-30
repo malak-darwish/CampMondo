3493e7e88754feadda167ec39ca87f19220c27ae
malak darwish <malakxdarwish@gmail.com>
2026-04-30 20:48:42 +0300

    connected database

--- backend/app/config.py
+++ backend/app/config.py
@@ -1,1 +1,10 @@
+import os
+
 SQLALCHEMY_DATABASE_URI = "mysql+pymysql://campmondo:password@localhost/campmondo"
+
+MAIL_SERVER =   os.getenv('MAIL_SERVER')
+MAIL_PORT =     int(os.getenv('MAIL_PORT', 587))
+MAIL_USE_TLS =  True
+MAIL_USERNAME = os.getenv('MAIL_USERNAME')
+MAIL_PASSWORD = os.getenv('MAIL_PASSWORD')
+MAIL_DEFAULT_SENDER = os.getenv('MAIL_DEFAULT_SENDER')
