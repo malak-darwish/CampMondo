7235a82e35103abc76aef414b2a6bc1ac68487a8
malak-darwish <malakxdarwish@gmail.com>
2026-05-10 22:16:25 +0300

    Admin AI Assistant

--- backend/run.py
+++ backend/run.py
@@ -1,8 +1,28 @@
 from app import create_app, db
+from flask_cors import CORS
+from dotenv import load_dotenv
+from routes.chatbot import chatbot_bp
+
+load_dotenv()
 
 app = create_app()
+
+CORS(app)
+
+# Register chatbot routes
+app.register_blueprint(
+    chatbot_bp,
+    url_prefix="/api/chatbot"
+)
+
+@app.route("/")
+def home():
+    return {
+        "message": "CampMondo Backend Running"
+    }
 if __name__ == "__main__":
+
     with app.app_context():
         db.create_all()
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    
 
